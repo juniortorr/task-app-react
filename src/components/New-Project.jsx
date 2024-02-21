ea3b9de502c20ae63a8a455dffc7a224bd6a0c21
juniortorr <juniortorr77@gmail.com>
2024-02-20 23:06:27 -0800

    set up new project section

--- src/components/New-Project.jsx
+++ src/components/New-Project.jsx
@@ -1,3 +1,47 @@
-function NewProject() {
-  return <section></section>;
+import Nav from './Nav';
+import styles from '../styles/New-Project.module.scss';
+import { Form } from 'react-router-dom';
+import storage from '../data';
+import { useNavigate } from 'react-router-dom';
+import Project from '../helpers/project';
+async function action() {
+  return console.log('done');
 }
+
+function NewProject() {
+  let navigate = useNavigate();
+
+  async function handleClick(e) {
+    const project = new Project(e.target[0].value);
+    await storage.addProject(project);
+    navigate('/');
+  }
+
+  return (
+    <>
+      <Nav></Nav>
+      <section>
+        <Form method="post" onSubmit={handleClick} className={styles.newProjectPopup}>
+          <input
+            name="project-name"
+            required
+            className={styles.title}
+            type="text"
+            placeholder="new project"
+          ></input>
+          <button type="button" className={styles.closePopup}>
+            X
+          </button>
+          <div className={styles.newProjectOptions}>
+            <button type="submit" className="createProject">
+              Create New Project
+            </button>
+          </div>
+        </Form>
+      </section>
+    </>
+  );
+}
+
+export default NewProject;
+export { action as formAction };
--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,15 @@ import {
   Route,
 } from 'react-router-dom';
 import App from './components/App.jsx';
+import NewProject from './components/New-Project.jsx';
 import { appLoader } from './loaders/app-loader';
 import './index.css';
-
+import { formAction } from './components/New-Project.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<App />} loader={appLoader}>
-        <Route path="/new-project" element={<div>New Project</div>}></Route>
-      </Route>
+      <Route path="/" element={<App />} loader={appLoader}></Route>
+      <Route path="/new-project" element={<NewProject />} action={formAction}></Route>
     </>
   )
 );
