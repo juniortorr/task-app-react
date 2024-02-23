cbcb322411e74b16acc0bbb3043fe0fa45ad8f6a
juniortorr <juniortorr77@gmail.com>
2024-02-22 23:19:45 -0800

    - add in loader for edit task route

--- src/loaders/app-loader.js
+++ src/loaders/app-loader.js
@@ -1,15 +1,22 @@
 import storage from '../data';
 
 async function loader() {
-  const tasks = await storage.getTasks();
+  const tasks = await storage.getProjects();
   return { tasks };
 }
 
 async function newTaskLoader({ params }) {
   const id = Number(params.projectId);
-  const project = await storage.getTaskById(id);
-  console.log({ 'New Task Loader': 'Fetch Project by ID Sucess!' });
+  const project = await storage.getProjectById(id);
+  console.log({ 'New Task Loader': 'Fetch Project by ID Sucess!', project: project });
   return { project };
 }
 
-export { loader as appLoader, newTaskLoader };
+async function editTaskLoader({ params }) {
+  const projectId = Number(params.projectId);
+  const taskId = Number(params.taskId);
+  const project = await storage.getProjectById(projectId);
+  const task = await project.getTaskById(taskId);
+  return { project, task };
+}
+export { loader as appLoader, newTaskLoader, editTaskLoader };
