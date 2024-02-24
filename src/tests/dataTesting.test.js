baef58b83d7ae55b9448e3a6cfd253cc0dd666de
juniortorr <juniortorr77@gmail.com>
2024-02-23 23:29:16 -0800

    -set up test for upcoming projects

--- src/tests/dataTesting.test.js
+++ src/tests/dataTesting.test.js
@@ -1,4 +1,4 @@
-import { format, compareAsc, addDays } from 'date-fns';
+import { format, compareAsc, addDays, differenceInCalendarDays } from 'date-fns';
 import { it, describe, expect } from 'vitest';
 
 describe('returns date', () => {
@@ -16,8 +16,20 @@ describe('returns sorted array', () => {
 
 describe('add day', () => {
   it('returns day added', () => {
-    const day = addDays('1/1/2024', 1);
+    const day = addDays('01/31/2024', 1);
     const response = format(day, 'MM/dd/yyyy');
-    expect(response).toBe('01/02/2024');
+    expect(response).toBe('02/01/2024');
+  });
+});
+
+describe('difference in days', () => {
+  it('returns true if date is 5 days away or less', () => {
+    let result;
+    const today = '02/01/2024';
+    const tomorrow = '02/03/2024';
+    if (differenceInCalendarDays(today, tomorrow) <= 5) {
+      result = true;
+    }
+    expect(result).toBeTruthy;
   });
 });
