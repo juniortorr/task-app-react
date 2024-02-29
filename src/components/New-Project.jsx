dc6e83ea1925fe08ac0b0c7612da23ec34acb005
juniortorr <juniortorr77@gmail.com>
2024-02-28 23:54:53 -0800

    - add in animations for new project page

--- src/components/New-Project.jsx
+++ src/components/New-Project.jsx
@@ -3,10 +3,12 @@ import { Form, useOutletContext } from 'react-router-dom';
 import storage from '../data';
 import { useNavigate } from 'react-router-dom';
 import Project from '../helpers/project';
+import { useState } from 'react';
 
 function NewProject() {
   // eslint-disable-next-line no-unused-vars
   const [status, setAlertStatus] = useOutletContext();
+  const [open, setOpen] = useState(true);
   let navigate = useNavigate();
   async function handleClick(e) {
     e.preventDefault();
@@ -19,27 +21,39 @@ function NewProject() {
     navigate('/');
   }
 
+  function handleClosePopup() {
+    setOpen(() => !open);
+    setTimeout(() => {
+      navigate('/');
+    }, 400);
+  }
+
   return (
     <>
-      <section>
-        <Form onSubmit={handleClick} className={styles.newProjectPopup}>
-          <input
-            name="project-name"
-            required
-            className={styles.title}
-            type="text"
-            placeholder="new project"
-          ></input>
-          <button type="button" className={styles.closePopup}>
-            X
+      <Form
+        onSubmit={handleClick}
+        className={
+          open === true
+            ? `${styles.newProjectPopup} ${styles.slideUp}`
+            : `${styles.newProjectPopup} ${styles.slideOut}`
+        }
+      >
+        <input
+          name="project-name"
+          required
+          className={styles.title}
+          type="text"
+          placeholder="new project"
+        ></input>
+        <button onClick={handleClosePopup} type="button" className={styles.closePopup}>
+          X
+        </button>
+        <div className={styles.newProjectOptions}>
+          <button type="submit" className="createProject">
+            Create New Project
           </button>
-          <div className={styles.newProjectOptions}>
-            <button type="submit" className="createProject">
-              Create New Project
-            </button>
-          </div>
-        </Form>
-      </section>
+        </div>
+      </Form>
     </>
   );
 }
